b3e8c1e486b0740bd1a36f3d83423b697a69bed1
kadoyau <el.psi.congroo@gmail.com>
2018-07-01 05:03:48 +0900

    Hello.say()のテストを書く
    consoleで特定の文字列が呼ばれたことを確認する

--- test/index.test.js
+++ test/index.test.js
@@ -13,4 +13,14 @@ describe('Hello Class Test', () => {
   test('We can check if the name defined the class constructor', () => {
     expect(hello.name).toBe(name);
   });
+
+  test('We can check if console.log() called on say() method', () => {
+    const spy = jest.spyOn(console, 'log');
+    hello.say();
+
+    expect(spy).toHaveBeenCalledWith(`Hello, ${name} World!`);
+
+    spy.mockReset();
+    spy.mockRestore();
+  });
 });
